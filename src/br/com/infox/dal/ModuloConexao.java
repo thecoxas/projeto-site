746796097aa60fc24f918a0ffc273cdb8e522d83
Dev theCoxas <168282052+thecoxas@users.noreply.github.com>
2024-06-15 20:04:18 -0300

    dbinfox

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.infox.dal;
+import java.sql.*;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author welton
+ */
+public class ModuloConexao {
+    // Método responsavel por estabelecer conexão com o banco de dados.
+    public static Connection conector(){
+        Connection conexao = null;
+        // A linha abaixo chama o driver
+        String driver = "com.mysql.cj.jdbc.Driver";
+        // Armazenando informações referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "";
+        // Estabelecendo a conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+            
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+        
+    }
+    
+}
+
